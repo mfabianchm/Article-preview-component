110659cdc8e055961009831c7529586b866174e4
mfabianchm <mfchongm@hotmail.com>
2024-02-19 10:13:02 -0600

    Initial Commit

---
+++ Article-preview-component/Article_preview_componentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Article_preview_componentApp.swift
+//  Article-preview-component
+//
+//  Created by Marcos Chong on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Article_preview_componentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Article-preview-component/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Article-preview-component
+//
+//  Created by Marcos Chong on 19/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
